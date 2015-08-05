dc2399325f51ebba917440021bd6136e063833ed
Brenden <bmowens@umail.iu.edu>
2015-08-05 11:44:08 -0400

    Changed from using a local database to using a text file. Local db have a way of not working well when publishing a website and also they don't play well with my current subscription of Azure.

--- PersonalWebsite/Current.aspx.cs
+++ PersonalWebsite/Current.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.IO;
 
 namespace PersonalWebsite
 {
@@ -11,30 +12,32 @@ namespace PersonalWebsite
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            using (WebsiteEntities tables = new WebsiteEntities())
+            LiteralControl projectGallery = new LiteralControl();
+            string tableStart = "<table class=\"table table-striped table-hover \"><thead><tr><th>#</th><th>Name</th><th>Description</th><th>Type</th><th>Language</th><th>Code</th></tr></thead><tbody>";
+            projectGallery.Text += tableStart;
+            int count = 1;
+            string projectHTML;
+            var projectFile = Server.MapPath("~/App_Data/Projects.txt");
+            var lines = File.ReadAllLines(projectFile);
+            foreach (var line in lines)
             {
-                var currentProjects = tables.Projects.Where(x => x.Completed == false).Select(x => x);
-                LiteralControl projectGallery = new LiteralControl();
-                string tableStart = "<table class=\"table table-striped table-hover \"><thead><tr><th>#</th><th>Name</th><th>Description</th><th>Type</th><th>Language</th><th>Code</th></tr></thead><tbody>";
-                projectGallery.Text += tableStart;
-                int count = 1;
-                string projectHTML;
-                foreach (Project proj in currentProjects)
+                var splitLine = line.Split('$');
+                if (splitLine[4] == "False")
                 {
-                    if (count % 2 == 1)
-                    {
-                        projectHTML = "<tr><td>" + count.ToString() + "</td><td>" + proj.Name.ToString() + "</td><td>" + proj.Description.ToString() + "</td><td>" + proj.Type.ToString() + "</td><td>" + proj.Language.ToString() + "</td><td><a href=\"" + proj.Link.ToString() + "\"class=\"btn btn-success\">Source</a>";
-                    }
-                    else
-                    {
-                        projectHTML = "<tr class=info><td>" + count.ToString() + "</td><td>" + proj.Name.ToString() + "</td><td>" + proj.Description.ToString() + "</td><td>" + proj.Type.ToString() + "</td><td>" + proj.Language.ToString() + "</td><td><a href=\"" + proj.Link.ToString() + "\"class=\"btn btn-success\">Source</a>";
-                    }
-                    count++;
-                    projectGallery.Text += projectHTML;
+                        if (count % 2 == 1)
+                        {
+                            projectHTML = "<tr><td>" + count.ToString() + "</td><td>" + splitLine[0] + "</td><td>" + splitLine[1] + "</td><td>" + splitLine[2] + "</td><td>" + splitLine[3] + "</td><td><a href=\"" + splitLine[4] + "\"class=\"btn btn-success\">Source</a>";
+                        }
+                        else
+                        {
+                            projectHTML = "<tr class=info><td>" + count.ToString() + "</td><td>" + splitLine[0] + "</td><td>" + splitLine[1] + "</td><td>" + splitLine[2] + "</td><td>" + splitLine[3] + "</td><td><a href=\"" + splitLine[4] + "\"class=\"btn btn-success\">Source</a>";
+                        }
+                        count++;
+                        projectGallery.Text += projectHTML;
                 }
+            }
                 projectGallery.Text += "</tbody></table>";
                 currentProjectsDiv.Controls.Add(projectGallery);
-            }
         }
     }
 }
--- PersonalWebsite/Finished.aspx.cs
+++ PersonalWebsite/Finished.aspx.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.HtmlControls;
 using System.Web.UI.WebControls;
+using System.IO;
 
 namespace PersonalWebsite
 {
@@ -12,31 +13,35 @@ namespace PersonalWebsite
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            using(WebsiteEntities tables = new WebsiteEntities())
+
+            var projectFile = Server.MapPath("~/App_Data/Projects.txt");
+            var lines = File.ReadAllLines(projectFile);
+            LiteralControl projectGallery = new LiteralControl();
+            string tableStart = "<table class=\"table table-striped table-hover \"><thead><tr><th>#</th><th>Name</th><th>Description</th><th>Type</th><th>Language</th><th>Code</th></tr></thead><tbody>";
+            projectGallery.Text += tableStart;
+            int count = 1;
+            string projectHTML;
+            foreach (var line in lines)
             {
-                var finishedProjects = tables.Projects.Where(x => x.Completed == true).Select(x => x);
-                LiteralControl projectGallery = new LiteralControl();
-                string tableStart = "<table class=\"table table-striped table-hover \"><thead><tr><th>#</th><th>Name</th><th>Description</th><th>Type</th><th>Language</th><th>Code</th></tr></thead><tbody>";
-                projectGallery.Text += tableStart;
-                int count = 1;
-                string projectHTML;
-                foreach(Project proj in finishedProjects)
+                var splitLine = line.Split('$');
+                if (splitLine[4] == "True")
                 {
                     if (count % 2 == 1)
                     {
-                        projectHTML = @"<tr><td>" + count.ToString() + @"</td><td>" + proj.Name.ToString() + @"</td><td>" + proj.Description.ToString() + @"</td><td>" + proj.Type.ToString() + @"</td><td>" + proj.Language.ToString() + @"</td><td><a href=""" + proj.Link.ToString() + @"""class=""btn btn-success"">Source</a>";
+                        projectHTML = @"<tr><td>" + count.ToString() + @"</td><td>" + splitLine[0] + @"</td><td>" + splitLine[1] + @"</td><td>" + splitLine[2] + @"</td><td>" + splitLine[3] + @"</td><td><a href=""" + splitLine[4] + @"""class=""btn btn-success"">Source</a>";
                     }
                     else
                     {
-                        projectHTML = @"<tr class=""info""><td>" + count.ToString() + @"</td><td>" + proj.Name.ToString() + @"</td><td>" + proj.Description.ToString() + @"</td><td>" + proj.Type.ToString() + @"</td><td>" + proj.Language.ToString() + @"</td><td><a href=""" + proj.Link.ToString() + @"""class=""btn btn-success"">Source</a>";
+                        projectHTML = @"<tr class=""info""><td>" + count.ToString() + @"</td><td>" + splitLine[0] + @"</td><td>" + splitLine[1] + @"</td><td>" + splitLine[2] + @"</td><td>" + splitLine[3] + @"</td><td><a href=""" + splitLine[4] + @"""class=""btn btn-success"">Source</a>";
                     }
-                        count++;
+                    count++;
                     projectGallery.Text += projectHTML;
                 }
-                projectGallery.Text += @"</tbody></table>";
-                finishedProjectsDiv.Controls.Add(projectGallery);
-                
             }
+            projectGallery.Text += @"</tbody></table>";
+            finishedProjectsDiv.Controls.Add(projectGallery);
+
         }
+
     }
 }
